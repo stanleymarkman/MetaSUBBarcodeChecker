6b657d908373dc9f5da2b813e42a4f670c4fc14c
Stanley Markman <lowerlabstan@gmail.com>
2018-08-09 11:47:33 -0400

    Barcode bounding box sucessfully generated. Tweak threshold and blur

--- samplecodereader.py
+++ samplecodereader.py
@@ -25,11 +25,38 @@ def main():
     gradient = cv2.subtract(gradX, gradY)
     gradient = cv2.convertScaleAbs(gradient)
 
-    showImageWindow(gradient)
+    # blur and threshold the image
+    blurred = cv2.blur(gradient, (50, 50))
+    (_, thresh) = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
 
+    # construct a closing kernel and apply it to the thresholded image
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+    closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+    # perform a series of erosions and dilations
+    closed = cv2.erode(closed, None, iterations = 4)
+    closed = cv2.dilate(closed, None, iterations = 4)
+
+    # find the contours in the thresholded image, then sort the contours
+    # by their area, keeping only the largest one
+    cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+    # compute the rotated bounding box of the largest contour
+    rect = cv2.minAreaRect(c)
+    box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+    box = np.int0(box)
+
+    # draw a bounding box arounded the detected barcode
+    cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+
+    showImageWindow(image)
+
+
+#Shows image at half size, waits for any keystroke
 def showImageWindow(im):
     cv2.namedWindow("Barcode", cv2.WINDOW_NORMAL)
-    imS = cv2.resize(im, (0,0), fx=0.5, fy=0.5) 
+    imS = cv2.resize(im, (0,0), fx=0.5, fy=0.5)
     cv2.imshow("Barcode", imS)
     cv2.waitKey(0)
 
