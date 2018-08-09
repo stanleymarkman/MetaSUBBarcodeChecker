2b9044ea5b9b47680da4aed9cc3fa2ec21498b55
Stanley Markman <lowerlabstan@gmail.com>
2018-08-09 13:22:26 -0400

    Cropped image to text and displayed OCR outputs. Fairly working version

--- samplecodereader.py
+++ samplecodereader.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import pytesseract
 
 def main():
     ap = argparse.ArgumentParser()
@@ -11,8 +12,36 @@ def main():
     	help = "path to the image file")
     args = vars(ap.parse_args())
 
-    # load the image and convert it to grayscale
+    # load the image
     image = cv2.imread(args["image"])
+
+    #Attempt to create a bounding box using blur factor of 50 and threshold of 100
+
+    box = attemptBoundingBox(image, 50, 90)
+
+    print(box)
+
+    #cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+
+    cropped = crop_minAreaRect(image, box)
+    cropped = imutils.rotate_bound(cropped, -90)
+    cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+    (_, cropped) = cv2.threshold(cropped, 74, 255, cv2.THRESH_BINARY)
+
+    # construct a closing kernel and apply it to the thresholded image
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 3))
+    closed = cv2.morphologyEx(cropped, cv2.MORPH_CLOSE, kernel)
+
+    closed = closed[250:]
+
+    # perform a series of erosions and dilations
+    closed = cv2.erode(closed, None, iterations = 2)
+    closed = cv2.dilate(closed, None, iterations = 3)
+    showImageWindow(closed)
+    print(pytesseract.image_to_string(closed))
+
+
+def attemptBoundingBox(image, blurFactor, threshold):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # compute the Scharr gradient magnitude representation of the images
@@ -26,8 +55,8 @@ def main():
     gradient = cv2.convertScaleAbs(gradient)
 
     # blur and threshold the image
-    blurred = cv2.blur(gradient, (50, 50))
-    (_, thresh) = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
+    blurred = cv2.blur(gradient, (blurFactor, blurFactor))
+    (_, thresh) = cv2.threshold(blurred, threshold, 255, cv2.THRESH_BINARY)
 
     # construct a closing kernel and apply it to the thresholded image
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
@@ -44,21 +73,45 @@ def main():
     c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
     # compute the rotated bounding box of the largest contour
     rect = cv2.minAreaRect(c)
-    box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
-    box = np.int0(box)
+    return rect
 
-    # draw a bounding box arounded the detected barcode
-    cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
-
-    showImageWindow(image)
-
+    #box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+    #box = np.int0(box)
 
 #Shows image at half size, waits for any keystroke
 def showImageWindow(im):
     cv2.namedWindow("Barcode", cv2.WINDOW_NORMAL)
-    imS = cv2.resize(im, (0,0), fx=0.5, fy=0.5)
+    imS = cv2.resize(im, (0,0), fx=1, fy=1)
     cv2.imshow("Barcode", imS)
     cv2.waitKey(0)
+
+#crop image to bounding box
+def bboxcrop(img):
+    rows = np.any(img, axis=1)
+    cols = np.any(img, axis=0)
+    ymin, ymax = np.where(rows)[0][[0, -1]]
+    xmin, xmax = np.where(cols)[0][[0, -1]]
+    return img[ymin:ymax+1, xmin:xmax+1]
+
+def crop_minAreaRect(img, rect):
+
+    # rotate img
+    angle = rect[2]
+    rows,cols = img.shape[0], img.shape[1]
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
+    img_rot = cv2.warpAffine(img,M,(cols,rows))
+
+    # rotate bounding box
+    rect0 = (rect[0], rect[1], 0.0)
+    box = cv2.boxPoints(rect)
+    pts = np.int0(cv2.transform(np.array([box]), M))[0]
+    pts[pts < 0] = 0
+
+    # crop
+    img_crop = img_rot[pts[1][1]:pts[0][1],
+                       pts[1][0]:pts[2][0]]
+
+    return img_crop
 
 
 if __name__ == "__main__":
